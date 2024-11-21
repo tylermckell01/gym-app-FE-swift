330712435bf137dcde2d66844d4720d3b0ca3d76
Tyler McKell <mckelltyler@gmail.com>
2024-11-20 17:18:41 -0700

    puts NavBar into its own component, add full functionality to NavBar

---
+++ 4-LYFE/Navigation/AppDestination.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+enum AppDestination: Hashable {
+    case createAccount
+    case login
+    case homePage
+    case profile
+    case workoutHistory
+    case workouts
+    case exercises
+    case measure
+}
---
+++ 4-LYFE/Navigation/MainView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct MainView: View {
+    @State private var navigationPath: [AppDestination] = [] 
+
+    var body: some View {
+        NavigationStack(path: $navigationPath) {
+            LandingPage(navigationPath: $navigationPath)
+                .navigationDestination(for: AppDestination.self) { destination in
+                    switch destination {
+                    case .createAccount:
+                        CreateAccountPage(navigationPath: $navigationPath)
+                    case .login:
+                        LoginPage(navigationPath: $navigationPath)
+                    case .homePage:
+                        HomePage(navigationPath: $navigationPath)
+                    case .profile:
+                        ProfilePage(navigationPath: $navigationPath)
+                    case .workoutHistory:
+                        WorkoutHistoryPage(navigationPath: $navigationPath)
+                    case .workouts:
+                        WorkoutsPage()
+                    case .exercises:
+                        ExercisesPage(navigationPath: $navigationPath)
+                    case .measure:
+                        MeasurePage(navigationPath: $navigationPath)
+                }
+            }
+        }
+    }
+}
+
+#Preview{
+    MainView()
+}
+
